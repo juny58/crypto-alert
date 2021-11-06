0d83359cad15e4ec72392f6807de60f6838a2ec7
juny58 <sunnysyed007@gmail.com>
2021-11-06 22:18:03 +0530

    port to PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 4900
+const port = process.env.PORT || 4900
 const cors = require('cors')
 const axios = require('axios')
 app.use(cors())
